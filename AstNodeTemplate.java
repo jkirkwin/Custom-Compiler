5df457d72b7826d15dfccb1a1cb7c6ed5f361bce
Jamie Kirkwin <jkirkwin@uvic.ca>
2021-05-25 13:17:33 -0700

    Add a helper script to generate ASTNode subclasses
    The python script makes a copy of the AstNodeTemplate.java file in the
    ast/ package, and replaces the necessary class names in the class
    definition and constructor.

---
+++ AstNodeTemplate.java
@@ -0,0 +1,13 @@
+package ast;
+
+/**
+ * TODO Add a docstring
+ */
+public class AstNodeTemplate extends ASTNode {
+
+    public AstNodeTemplate() {
+        // TODO Add call to superclass constructor
+        // TODO Add any necessary parameters
+    }
+
+}
