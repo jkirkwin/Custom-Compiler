72ec9c2bd3b50cd51afbc73926eb92c635e87a5b
Jamie Kirkwin <jkirkwin@uvic.ca>
2021-05-27 15:52:30 -0700

    Add Expression AST node
    Expression is a placeholder for now. Later, it will be made abstract and
    subclassed by concrete expression types (e.g. array references and
    less-than expressions)

---
+++ ast/Expression.java
@@ -0,0 +1,15 @@
+package ast;
+
+/**
+ * TODO Add a docstring
+ */
+public class Expression extends ASTNode {
+
+    public Expression() {
+        // TODO Add call to superclass constructor
+        // TODO Add any necessary parameters
+    }
+
+    // TODO Should be made abstract once we're ready to add some sub-types
+
+}
