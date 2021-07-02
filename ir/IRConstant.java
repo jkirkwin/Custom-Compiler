1773254984aaf082d353d84eef90fbf0fceac347
Jamie Kirkwin <jkirkwin@uvic.ca>
2021-07-02 15:55:52 -0700

    Add IR constant representations
    Adds a generic class with factory methods to represent each type of
    constant allowed in the IR.

---
+++ ir/IRConstant.java
@@ -0,0 +1,58 @@
+package ir;
+
+import type.*;
+
+/**
+ * Represents a constant in the IR. There are a fixed number of possible
+ * contant types, and a factory method is provided for each one.
+ */
+public class IRConstant<T> {
+
+    public static IRConstant<Boolean> forBoolean(boolean value) {
+        return new IRConstant<Boolean>(type.BooleanType.INSTANCE, value);
+    }
+
+    public static IRConstant<Character> forCharacter(char value) {
+        return new IRConstant<Character>(type.CharacterType.INSTANCE, value);
+    }
+
+    public static IRConstant<Float> forFloat(float value) {
+        return new IRConstant<Float>(type.FloatType.INSTANCE, value);
+    }
+
+    public static IRConstant<Integer> forInteger(int value) {
+        return new IRConstant<Integer>(type.IntegerType.INSTANCE, value);
+    }
+
+    public static IRConstant<String> forString(String value) {
+        return new IRConstant<String>(type.StringType.INSTANCE, value);
+    }
+
+    private final SimpleType simpleType;
+    private final T value;
+
+    private IRConstant(SimpleType t, T value) {
+        assert t != null;
+        assert value != null;
+
+        simpleType = t;
+        this.value = value;
+    }
+
+    public SimpleType type() {
+        return simpleType;
+    }
+
+    public T value() {
+        return value;
+    }
+
+    public String toString() {
+        if (value instanceof Boolean) {
+            return value.toString().toUpperCase();
+        }
+        else {
+            return value.toString();
+        }
+    }
+}
