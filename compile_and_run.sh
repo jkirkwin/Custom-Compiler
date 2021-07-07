5a25738dcac394b0a8b905b9b2e7cb437bcb8e32
Jamie Kirkwin <jkirkwin@uvic.ca>
2021-07-07 12:16:16 -0700

    Add missing exits to helper compile script
    Exit the compile script when any phase fails.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -17,17 +17,18 @@ mkdir -p tmp
 BASE_FILE_NAME=$(basename $UL_FILE .ul)
 IR_FILE=${TEMP_DIR}/${BASE_FILE_NAME}.ir
 
-
 java Compiler $UL_FILE 2>/dev/null >$IR_FILE
 
 if [ $? -ne 0 ]; then
     echo "Compilation phase failed"
+    exit 1;
 fi
 
 ./ir_to_class.sh $IR_FILE $TEMP_DIR/
 
 if [ $? -ne 0 ]; then
     echo "IR->class translation failed"
+    exit 1;
 fi
 
 java -cp $TEMP_DIR $CLASS_NAME
